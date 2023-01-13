91901bf1e4b650e2121f5e9d1c81bab4985a74a2
Boyan Todorov <106776456+bopzen@users.noreply.github.com>
2023-01-13 16:23:10 +0200

    Add files via upload

---
+++ 03 C# Advanced/08 Streams, Files and Directories - Lab/07. Folder Size/FolderSize.cs
@@ -0,0 +1,29 @@
+﻿namespace FolderSize
+{
+    using System;
+    using System.IO;
+    public class FolderSize
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = @"..\..\..\Files\TestFolder";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            GetFolderSize(folderPath, outputPath);
+        }
+
+        public static void GetFolderSize(string folderPath, string outputFilePath)
+        {
+            double sum = 0;
+            DirectoryInfo dir = new DirectoryInfo(folderPath);
+            FileInfo[] files = dir.GetFiles("*", SearchOption.AllDirectories);
+            foreach (FileInfo file in files)
+            {
+                sum += file.Length;
+            }
+
+            sum = sum / 1024;
+            File.WriteAllText(outputFilePath, $"{sum} KB");
+        }
+    }
+}
