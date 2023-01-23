6f2e10e631c0b2a2682ca81337202ef7f1a9d428
Boyan Todorov <106776456+bopzen@users.noreply.github.com>
2023-01-23 11:20:23 +0200

    Add files via upload

---
+++ 03 C# Advanced/15 Generics - Lab/01. Box/Box.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        public Box()
+        {
+            list = new Stack<T>();
+        }
+        private Stack<T> list;
+        public int Count { get { return list.Count; } }
+
+        public void Add(T element)
+        {
+            list.Push(element);
+        }
+        public T Remove()
+        {
+            return list.Pop();
+        }
+    }
+}
---
+++ 03 C# Advanced/15 Generics - Lab/02. Array Creator/ArrayCreator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericArrayCreator
+{
+    public class ArrayCreator
+    {
+        public static T[] Create<T>(int length, T item)
+        {
+            T[] internalArray = new T[length];
+            for (int i = 0; i < internalArray.Length; i++)
+            {
+                internalArray[i] = item;
+            }
+            return internalArray;
+        }
+    }
+}
