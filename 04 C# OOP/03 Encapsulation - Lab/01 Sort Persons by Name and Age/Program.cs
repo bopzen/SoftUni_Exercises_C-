127da4247cb1659fdeeb3043d5b662a0f03e0914
Boyan Todorov <106776456+bopzen@users.noreply.github.com>
2023-02-20 12:58:04 +0200

    Add files via upload

---
+++ 04 C# OOP/03 Encapsulation - Lab/01 Sort Persons by Name and Age/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            List<Person> persons = new List<Person>();
+            for (int i = 0; i < lines; i++)
+            {
+                string[] tokens = Console.ReadLine().Split();
+                Person person = new Person(tokens[0], tokens[1], int.Parse(tokens[2]));
+                persons.Add(person);
+            }
+            persons.OrderBy(p => p.FirstName).ThenBy(p => p.Age).ToList().ForEach(p => Console.WriteLine(p.ToString()));
+        }
+    }
+}
