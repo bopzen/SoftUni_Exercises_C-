a14cda7c8c24306372ae4fc7a59e922fe38f19ad
Boyan Todorov <106776456+bopzen@users.noreply.github.com>
2023-02-21 10:58:52 +0200

    Add files via upload

---
+++ 04 C# OOP/05 Interfaces and Abstraction - Lab/01 Shapes/Rectangle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        private int height;
+        private int width;
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public int Width
+        {
+            get { return this.width; }
+            set { this.width = value; }
+        }
+        public int Height
+        {
+            get { return this.height; }
+            set { this.height = value; }
+        }
+        public void Draw()
+        {
+            DrawLine(this.width, '*', '*');
+            for (int i = 1; i < this.height - 1; ++i)
+            {
+                DrawLine(this.width, '*', ' ');
+            }
+            DrawLine(this.width, '*', '*');
+        }
+
+        private void DrawLine(int width, char end, char mid)
+        {
+            Console.Write(end);
+            for (int i = 1; i < width - 1; ++i)
+            {
+                Console.Write(mid);
+            }
+            Console.WriteLine(end);
+        }
+    }
+}
